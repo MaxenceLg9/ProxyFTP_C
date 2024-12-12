14d3ba84953d69633f9585fea7a89d376e4dac0f
maxence <maxence@maxence.maxence.net>
2024-12-12 10:25:52 +0100

    First commit : creating the proxy file. Goal is to intercept and redirect respecting given specific conditions to the server or abort connection.

---
+++ proxy.c
@@ -0,0 +1,11 @@
+#include  <stdio.h>
+#include  <stdlib.h>
+#include  <sys/socket.h>
+#include  <netdb.h>
+#include  <string.h>
+#include  <unistd.h>
+#include  <stdbool.h>
+
+int main(int argc, const char** argv){
+	
+}
